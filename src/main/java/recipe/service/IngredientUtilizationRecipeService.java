4c237dfc7508a22dbb316b3eb2c639ec9b4cec3e
YoonJoony <dbswns1101@naver.com>
2024-07-28 23:47:56 +0900

    feat(#8) : 식재료 조합 레시피 기능 추가

---
+++ src/main/java/recipe/service/IngredientUtilizationRecipeService.java
@@ -0,0 +1,43 @@
+package recipe.service;
+
+import oracle.jdbc.OracleTypes;
+import oracle.sql.ARRAY;
+import oracle.sql.ArrayDescriptor;
+import recipe.constant.Category;
+import recipe.constant.Difficulty;
+import recipe.domain.Recipe;
+
+import java.sql.*;
+import java.util.*;
+
+public class IngredientUtilizationRecipeService {
+    public List<Recipe> getIngredientUtilizationRecipes(String[] ingredientsArray, Connection conn) throws SQLException {
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        Recipe recipe = null;
+
+        ArrayDescriptor descriptor = ArrayDescriptor.createDescriptor("INGREDIENT_ARRAY", conn);
+        ARRAY ingredients = new ARRAY(descriptor, conn, ingredientsArray);
+        String sql = "{? = call ingredient_combination(?)}";
+
+        CallableStatement cStmt = conn.prepareCall(sql);
+        cStmt.registerOutParameter(1, OracleTypes.CURSOR);
+        cStmt.setArray(2, ingredients);
+        cStmt.execute();
+        ResultSet rs = (ResultSet) cStmt.getObject(1);
+
+        while (rs.next()) {
+            recipe = Recipe.builder()
+                    .id(rs.getLong("recipe_id"))
+                    .memberId(rs.getLong("member_id"))
+                    .category(Category.fromDescription(rs.getString("category")))
+                    .title(rs.getString("recipe_name"))
+                    .description(rs.getString("description"))
+                    .difficulty(Difficulty.fromDescription(rs.getString("difficulty")))
+                    .quantity(rs.getString("quantity"))
+                    .build();
+            recipes.add(recipe);
+        }
+        return recipes;
+    }
+
+}
