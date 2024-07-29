a12cd1c5f8b5f5fc7c12b8965ba577b705330ecf
이효주 <dlgywn2420@gmail.com>
2024-07-29 16:22:57 +0900

    feat(#16): role enum 타입 필드 값을 기반으로 값을 변경하는 static method 추가

--- src/main/java/user/constant/Role.java
+++ src/main/java/user/constant/Role.java
@@ -11,4 +11,13 @@ public enum Role {
 	Role(String type) {
 		this.type = type;
 	}
+
+	public static Role fromType(String type) {
+		for (Role role : Role.values()) {
+			if (role.getType().equals(type)) {
+				return role;
+			}
+		}
+		throw new IllegalArgumentException("No enum constant with type: " + type);
+	}
 }
