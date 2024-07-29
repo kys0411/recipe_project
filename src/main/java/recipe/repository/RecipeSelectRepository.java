2d4065c06405ee9c7f4a4ee31e3a6e0df1e064bc
YoonJoony <dbswns1101@gmail.com>
2024-07-29 14:31:54 +0900

    refactor(#23) : 데이터 타입 일관성에 맞게 수정 및 메소드 수정

--- src/main/java/recipe/repository/RecipeSelectRepository.java
+++ src/main/java/recipe/repository/RecipeSelectRepository.java
@@ -17,60 +17,53 @@ import java.util.List;
 public class RecipeSelectRepository {
     private final DBConnection dbConnection;
 
-    public ArrayList<Recipe> selectAllRecipe() throws SQLException, ClassNotFoundException {
+    public List<Recipe> selectAllRecipe() throws SQLException, ClassNotFoundException {
         Connection conn = dbConnection.getConnection();
-        ArrayList<Recipe> recipes = new ArrayList<>();
-        Recipe recipe = null;
 
         String selectSQL = "SELECT * FROM recipe";
 
         PreparedStatement pStmt = conn.prepareStatement(selectSQL);
         ResultSet rs = pStmt.executeQuery();
 
-        while(rs.next()) {
-            recipe = Recipe.builder()
-                    .id(rs.getLong("recipe_id"))
-                    .memberId(rs.getLong("member_id"))
-                    .category(Category.fromDescription(rs.getString("category")))
-                    .title(rs.getString("recipe_name"))
-                    .description(rs.getString("description"))
-                    .difficulty(Difficulty.fromDescription(rs.getString("difficulty")))
-                    .quantity(rs.getString("quantity"))
-                    .build();
-
-            recipes.add(recipe);    // create Recipe object and add to recipes
-        }
-
-        return recipes;
+        return returnRecipeListInfo(rs);
     }
 
     public List<Recipe> getIngredientCombinationRecipeInfo(String[] ingredientsArray) throws SQLException, ClassNotFoundException {
-        List<Recipe> recipes = new ArrayList<>();
         Connection conn = dbConnection.getConnection();
 
         ArrayDescriptor descriptor = ArrayDescriptor.createDescriptor("INGREDIENT_ARRAY", conn);
         ARRAY ingredients = new ARRAY(descriptor, conn, ingredientsArray);
 
         String sql = "{? = call ingredient_combination(?)}";
-        CallableStatement cStmt = conn.prepareCall(sql);
 
+        CallableStatement cStmt = conn.prepareCall(sql);
         cStmt.registerOutParameter(1, OracleTypes.CURSOR);
         cStmt.setArray(2, ingredients);
         cStmt.execute();
 
         ResultSet rs = (ResultSet) cStmt.getObject(1);
 
-        while (rs.next()) {
-            Recipe recipe = Recipe.builder()
-                    .id(rs.getLong("recipe_id"))
-                    .memberId(rs.getLong("member_id"))
-                    .category(Category.fromDescription(rs.getString("category")))
-                    .title(rs.getString("recipe_name"))
-                    .description(rs.getString("description"))
-                    .difficulty(Difficulty.fromDescription(rs.getString("difficulty")))
-                    .quantity(rs.getString("quantity"))
-                    .build();
-            recipes.add(recipe);
+        return returnRecipeListInfo(rs);
+    }
+
+    public List<Recipe> returnRecipeListInfo(ResultSet rs) {
+        List<Recipe> recipes = new ArrayList<>();
+
+        try {
+            while (rs.next()) {
+                Recipe recipe = Recipe.builder()
+                        .id(rs.getLong("recipe_id"))
+                        .memberId(rs.getLong("member_id"))
+                        .category(Category.fromDescription(rs.getString("category")))
+                        .title(rs.getString("recipe_name"))
+                        .description(rs.getString("description"))
+                        .difficulty(Difficulty.fromDescription(rs.getString("difficulty")))
+                        .quantity(rs.getString("quantity"))
+                        .build();
+                recipes.add(recipe);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
         return recipes;
