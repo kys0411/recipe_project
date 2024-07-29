df31a2a3e06d2a2e4d672877ed452511bb747fca
YoonJoony <dbswns1101@gmail.com>
2024-07-29 14:28:31 +0900

    refactor(#23) : 레시피 상태 변경 작업과 조회 작업을 분리 하기위해 코드 변경

--- src/main/java/recipe/service/IngredientCombinationService.java
+++ src/main/java/recipe/service/IngredientCombinationService.java
@@ -2,18 +2,18 @@ package recipe.service;
 
 import lombok.RequiredArgsConstructor;
 import recipe.domain.Recipe;
-import recipe.repository.RecipeRepository;
+import recipe.repository.RecipeSelectRepository;
 
 import java.sql.*;
 import java.util.*;
 
 @RequiredArgsConstructor
 public class IngredientCombinationService {
-    private final RecipeRepository recipeRepository;
+    private final RecipeSelectRepository recipeSelectRepository;
 
     public List<Recipe> ingredientCombination(String[] ingredientsArray) {
         try {
-            return recipeRepository.getIngredientCombinationRecipeInfo(ingredientsArray);
+            return recipeSelectRepository.getIngredientCombinationRecipeInfo(ingredientsArray);
         } catch (SQLException | ClassNotFoundException e) {
             // todo: 예외 로직 처리
         }
