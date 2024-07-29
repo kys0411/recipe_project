68c109e2c86382ca63b18037a718547e1c3fd55d
이효주 <dlgywn2420@gmail.com>
2024-07-29 14:54:49 +0900

    refactor(#9): enum으로 변경

--- src/main/java/user/service/UserService.java
+++ src/main/java/user/service/UserService.java
@@ -1,6 +1,7 @@
 package user.service;
 
 import user.DBConnection;
+import user.constant.Role;
 import user.domain.User;
 import user.dto.UserjoinRequestDto;
 
@@ -15,7 +16,7 @@ public class UserService {
             String sql = "insert into member(member_id, nickname, role, hint, password) values(SEQ_MEMBER_ID.nextval, ?, ?, ?, ?)";
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, userjoinRequestDto.getNickname());
-            pstmt.setString(2, "1");
+            pstmt.setString(2, Role.NORMAL.getRoleType());
             pstmt.setString(3, userjoinRequestDto.getHint());
             pstmt.setString(4, userjoinRequestDto.getPassword());
             pstmt.executeUpdate();
