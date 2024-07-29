421e9944f467ae6f0f5b84e981c2c8fb1620af4b
YoonJoony <dbswns1101@gmail.com>
2024-07-29 14:15:16 +0900

    refactor(#23) : 이번 수정 작업 변경과 함께 Test 코드 수정

--- src/test/java/recipe/combination/RecipeCombinationTest.java
+++ src/test/java/recipe/combination/RecipeCombinationTest.java
@@ -3,14 +3,14 @@ package recipe.combination;
 import common.DBConnection;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
-import recipe.repository.RecipeRepository;
+import recipe.repository.RecipeSelectRepository;
 
 import java.sql.SQLException;
 
 
 public class RecipeCombinationTest {
     DBConnection dbConnection = new DBConnection();
-    RecipeRepository recipeRepository = new RecipeRepository(dbConnection);
+    RecipeSelectRepository recipeSelectRepository = new RecipeSelectRepository(dbConnection);
 
     @Test
     @DisplayName("식재료 조합 레시피 테스트")
@@ -19,6 +19,6 @@ public class RecipeCombinationTest {
         String[] ingredientsArray = {"우유", "양파", "모짜렐라치즈"};
         // When, Then
         Assertions.assertThatNoException()
-                .isThrownBy(() -> recipeRepository.getIngredientCombinationRecipeInfo(ingredientsArray));
+                .isThrownBy(() -> recipeSelectRepository.getIngredientCombinationRecipeInfo(ingredientsArray));
     }
 }
