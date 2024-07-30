e0030176b64b6db91b93ca4afa2a53510ee66302
smuhsh <smu_hsh@naver.com>
2024-07-30 14:04:43 +0900

    리뷰후기 추천점수 시각화,체크박스 선택 이벤트 작업

--- src/main/java/review/impl/ReviewDaoImpl.java
+++ src/main/java/review/impl/ReviewDaoImpl.java
@@ -1,43 +1,30 @@
 package review.impl;
 
-import oracle.jdbc.driver.OracleDriver;
+import common.DBConnection;
+import javafx.scene.control.CheckBox;
 import review.Dao.ReviewDao;
 import review.domain.Review;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ReviewDaoImpl implements ReviewDao {
 
-    // 데이터베이스 연결을 가져오는 메서드
-    private static Connection getConnection() throws Exception {
-        Map<String, String> env = System.getenv();
-
-        OracleDriver driver = new OracleDriver();
-        DriverManager.registerDriver(driver);
-
-        Class.forName("oracle.jdbc.OracleDriver");
-        String url = env.get("DB_URL");
-        String id = env.get("DB_ID");
-        String pass = env.get("DB_PASS");
-
-        return DriverManager.getConnection(url, id, pass);
-    }
+    private DBConnection dbConnection = new DBConnection();
 
     @Override
     public List<Review> selectMemberReview(long memberId) throws Exception {
         List<Review> reviews = new ArrayList<>();
 
-        String sql = "SELECT r.review_id, r.member_id, r.recipe_id, c.recipe_name, m.nickname, r.rating, r.content, r.review_date " +
+        String sql = "SELECT r.review_id, r.member_id, r.recipe_id, c.recipe_name, m.nickname, " +
+                "LPAD('★', r.rating, '★') AS star_rating, r.content, r.review_date " +
                 "FROM member m, review r, recipe c " +
                 "WHERE r.member_id = m.member_id(+) AND r.recipe_id = c.recipe_id(+) AND r.member_id = ?";
 
-        try (Connection conn = getConnection();
+        try (Connection conn = dbConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)){
 
              pstmt.setLong(1,memberId);
@@ -50,9 +37,10 @@ public class ReviewDaoImpl implements ReviewDao {
                 review.setRecipeId(rs.getLong("recipe_id"));
                 review.setRecipeName(rs.getString("recipe_name"));
                 review.setNickName(rs.getString("nickname"));
-                review.setRating(rs.getLong("rating"));
+                review.setStarRating(rs.getString("star_rating"));  // 새로운 star_rating 열 추가
                 review.setContent(rs.getString("content"));
                 review.setDate(rs.getDate("review_date"));
+                review.setCbDelete(new CheckBox());  // CheckBox 초기화 추가
 
                 reviews.add(review);
             }
