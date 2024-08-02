e8ce035efe0d8ba8881b468754399bf06a550c78
YoonJoony <dbswns1101@gmail.com>
2024-08-02 16:31:31 +0900

    feature/#78 : 식재료 조합 레시피 목록 조회 UI 구현

--- src/main/java/recipe/controller/ingredientCombinationController.java
+++ src/main/java/recipe/controller/ingredientCombinationController.java
@@ -4,11 +4,15 @@ import common.DBConnection;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,6 +22,7 @@ import recipe.service.IngredientCombinationService;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -36,6 +41,9 @@ public class ingredientCombinationController implements Initializable {
 
     @FXML
     private HBox searchIngredeintListHBox;
+
+    @FXML
+    private VBox recipeContainer;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -66,10 +74,67 @@ public class ingredientCombinationController implements Initializable {
         searchField.setText("");
         selectCombinationRecipeList(ingredients);
     }
-    
+
     // 식재료 조합 레시피 조회
     public void selectCombinationRecipeList(String[] ingredients) {
+        recipeContainer.getChildren().clear();
+
         List<Recipe> recipeList = ingredientCombinationService.ingredientCombination(ingredients);
 
+        for (Recipe recipe : recipeList) {
+            VBox vBox = new VBox();
+            HBox hBox = new HBox();
+
+            VBox imageBox = new VBox();
+            String relativePath = "defaultRecipeImage.jpg";
+
+            Image image = new Image(relativePath);
+            ImageView recipeImage = new ImageView(image);
+            recipeImage.setFitWidth(130);
+            recipeImage.setFitHeight(130);
+
+            imageBox.getChildren().add(recipeImage);
+
+            VBox contentBox = new VBox();
+
+            HBox headerBox = new HBox();
+            Label title = new Label();
+            title.setText(recipe.getTitle());
+            title.setStyle("-fx-font-size: 20px; -fx-padding: 0 14px 2px 0;");
+
+
+            Label createDate = new Label();
+            createDate.setText(recipe.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+            headerBox.getChildren().addAll(title, createDate);
+            HBox.setMargin(createDate, new Insets(0, 0, 0, 0));
+
+            Label description = new Label();
+            description.setText(recipe.getDescription());
+
+            HBox footerBox = new HBox();
+            Label difficulty = new Label();
+            difficulty.setText("난이도 : " + recipe.getDifficulty());
+            Label reviews = new Label();
+            reviews.setText("후기 : " + "10" + "개 " + "(" + 2.5 + ")");
+            Label likes = new Label();
+            likes.setText(5 +  " likes");
+
+            footerBox.getChildren().addAll(difficulty, reviews, likes);
+            HBox.setMargin(difficulty, new Insets(0, 50, 0, 0));
+            HBox.setMargin(reviews, new Insets(0, 50, 0, 0));
+
+            contentBox.getChildren().addAll(headerBox, description, footerBox);
+            VBox.setMargin(headerBox, new Insets(15, 0, 0, 0));
+            VBox.setMargin(description, new Insets(20, 0, 20, 0));
+
+            hBox.getChildren().addAll(imageBox, contentBox);
+            HBox.setMargin(contentBox, new Insets(0, 0, 0, 20));
+            vBox.getChildren().add(hBox);
+            VBox.setMargin(hBox, new Insets(10, 0, 0, 0));
+
+            recipeContainer.getChildren().add(vBox);
+            VBox.setMargin(vBox, new Insets(20, 0, 0, 10));
+        }
     }
 }
