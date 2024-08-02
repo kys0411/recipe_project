72e1b9678dc7175e8fbb97b9a8ecfa45862dda5b
smuhsh <smu_hsh@naver.com>
2024-08-02 14:18:28 +0900

    feature/76

--- src/main/java/recipe/repository/RecipeQueryRepository.java
+++ src/main/java/recipe/repository/RecipeQueryRepository.java
@@ -125,4 +125,31 @@ public class RecipeQueryRepository {
 
         return recipes;
     }
+
+    public List<RecipeDto.FindAll> findRecipeByKeyword(String keyword) throws SQLException, ClassNotFoundException {
+        List<RecipeDto.FindAll> recipes = new ArrayList<>();
+        Connection conn = dbConnection.getConnection();
+
+        String sql = "SELECT * FROM recipe WHERE recipe_name LIKE ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, "%" + keyword + "%");
+
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            RecipeDto.FindAll recipe = RecipeDto.FindAll.builder()
+                    .id(rs.getLong("recipe_id"))
+                    .title(rs.getString("recipe_name"))
+                    .difficulty(rs.getString("difficulty"))
+                    .build();
+            recipes.add(recipe);
+        }
+
+        conn.close();
+        pstmt.close();
+        rs.close();
+
+        return recipes;
+    }
 }
